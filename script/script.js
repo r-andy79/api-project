b5473384b0f6b0a34dbd722a43ed475e8e3ee2cb
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2022-04-08 09:54:49 +0200

    Pagination added

--- script/script.js
+++ script/script.js
@@ -1,22 +1,76 @@
+import Wrapper from './fetch-wrapper.js'
+
+const API = new Wrapper('https://api.punkapi.com/v2/beers?page=')
+
 const btnEl = document.querySelector('button');
 const article = document.querySelector('article');
+
+let currentPageNumber = 1;
+// let prevPageNumber;
+// let nextPageNumber = currentPageNumber + 1;
+
+
+
+const getData = (endpoint => {
+  API.get(endpoint)
+    .then(data => {
+      console.log(data)
+      if (data.length > 0) {
+        data.forEach(item => {
+          insertData(item);
+        })
+        // currentPageNumber += 1;
+        // console.log(currentPageNumber);
+        article.appendChild(prevBtn);
+        article.appendChild(nextBtn);
+      } else if (data.length === 0) {
+        console.log(data)
+        // currentPageNumber -= 1;
+        article.insertAdjacentHTML('beforeend', `<p>No more beers to show :(. Please go back to the previous page</p>`)
+        article.appendChild(prevBtn)
+      }
+    })
+})
+
+const nextBtn = document.createElement("button")
+nextBtn.textContent = 'Next page';
+
+const prevBtn = document.createElement("button");
+prevBtn.textContent = 'Prev page';
+
+// function inserting data to the website
+
+const insertData = (item) => {
+  article.insertAdjacentHTML(
+    'beforeend',
+    `<h2>${item.name}</h2>
+    <p>Alcohol: <span>${item.abv}%</span>, IBU: <span>${item.ibu}</span></p>
+    <p>${item.tagline}</p>
+    <p>${item.description}</p>
+    <p>It goes great with: ${item.food_pairing}</p>
+    `
+  )
+}
 
 
 btnEl.addEventListener('click', () => {
   article.innerHTML = "";
-  fetch('https://api.punkapi.com/v2/beers/random')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data[0]);
-    article.insertAdjacentHTML(
-      'afterbegin',
-      `<h2>${data[0].name}</h2>
-      <p>Alcohol: <span>${data[0].abv}%</span>, IBU: <span>${data[0].ibu}</span></p>
-       <img src=${data[0].image_url}>
-       <p>${data[0].tagline}</p>
-       <p>${data[0].description}</p>
-       <p>It goes great with: ${data[0].food_pairing}</p>
-      `
-      )
-  })
+  getData(currentPageNumber)
+})
+
+nextBtn?.addEventListener('click', () => {
+  article.innerHTML = "";
+  getData(currentPageNumber + 1);
+  currentPageNumber += 1;
+})
+
+prevBtn?.addEventListener('click', () => {
+  console.log(currentPageNumber)
+  article.innerHTML = "";
+  getData(currentPageNumber - 1);
+  if (currentPageNumber === 1) {
+    prevPageNumber = currentPageNumber;
+  } else {
+    prevPageNumber = currentPageNumber - 1;
+  }
 })
