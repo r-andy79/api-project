be269b5fc3190b5f7a73cc350628a93e2876bf61
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2022-04-12 08:54:41 +0200

    added 'select' element to specify the results number

--- script/script.js
+++ script/script.js
@@ -4,16 +4,22 @@ const API = new Wrapper('https://api.punkapi.com/v2/beers?page=')
 
 const btnEl = document.querySelector('button');
 const article = document.querySelector('article');
+const selectEL = document.querySelector('select');
 
 let currentPageNumber = 1;
 let prevPageNumber;
-// let nextPageNumber = currentPageNumber + 1;
+let resultsPerPage = selectEL.value;
 
+selectEL.addEventListener('change', () => {
+  resultsPerPage = selectEL.value;
+  return resultsPerPage;
+})
 
 
 const getData = (endpoint => {
   API.get(endpoint)
     .then(data => {
+      console.log(data)
       if (data.length > 0) {
         data.forEach(item => {
           insertData(item);
@@ -44,10 +50,10 @@ const insertData = (item) => {
   article.insertAdjacentHTML(
     'beforeend',
     `<h2>${item.name}</h2>
-    <p>Alcohol: <span>${item.abv}%</span>, IBU: <span>${item.ibu}</span></p>
-    <p>${item.tagline}</p>
-    <p>${item.description}</p>
-    <p>It goes great with: ${item.food_pairing}</p>
+      <p>Alcohol: <span>${item.abv}%</span>, IBU: <span>${item.ibu}</span></p>
+      <p>${item.tagline}</p>
+      <p>${item.description}</p>
+      <p>It goes great with: ${item.food_pairing}</p>
     `
   )
 }
@@ -55,23 +61,22 @@ const insertData = (item) => {
 
 btnEl.addEventListener('click', () => {
   article.innerHTML = "";
-  getData(currentPageNumber)
+  getData(`${currentPageNumber}&per_page=${resultsPerPage}`)
 })
 
 nextBtn?.addEventListener('click', () => {
   article.innerHTML = "";
-  getData(currentPageNumber + 1);
+  getData(`${currentPageNumber + 1}&per_page=${resultsPerPage}`);
   currentPageNumber += 1;
 })
 
 prevBtn?.addEventListener('click', () => {
-  console.log(currentPageNumber)
   article.innerHTML = "";
   if (currentPageNumber === 1) {
     prevPageNumber = currentPageNumber;
   } else {
     prevPageNumber = currentPageNumber - 1;
   }
-  getData(prevPageNumber);
+  getData(`${prevPageNumber}&per_page=${resultsPerPage}`);
   currentPageNumber -= 1;
 })
